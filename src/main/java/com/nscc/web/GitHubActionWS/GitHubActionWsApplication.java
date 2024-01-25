0c657c47fb60b1ba6bc6aa08fb8d3519cac1c998
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-01-25 15:14:38 -0400

    first commit

---
+++ src/main/java/com/nscc/web/GitHubActionWS/GitHubActionWsApplication.java
@@ -0,0 +1,13 @@
+package com.nscc.web.GitHubActionWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nscc/web/GitHubActionWS/GitHubActionWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nscc.web.GitHubActionWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
